2adcf0cf9d6c5102ab449ff30d6d1a0026efb358
muniafu <elambaraka2019@gmail.com>
2024-03-24 05:45:50 -0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DealController;
+use Illuminate\Support\Facades\Route;
 
 // Route for creating a new deal
 Route::post('/deals', [DealController::class, 'create'])->name('deals.create');
